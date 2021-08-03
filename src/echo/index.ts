83b09a5e45c73ec8dc359670b831b989b7375d5a
cn-devflows-prod <73590877+cn-devflows-prod@users.noreply.github.com>
2021-08-03 20:37:31 +0530

    Initial commit

---
+++ src/echo/index.ts
@@ -0,0 +1,44 @@
+/**
+ * Sample action "echo" - remove/reuse if not required
+ *
+ * The following interfaces would match with your input/output schema as set on the DevFlows UI
+ */
+interface ActionInput {
+  // Echo can have any kind of input
+  body: unknown;
+  headers?: {
+    [key: string]: string;
+  };
+}
+
+interface ActionOutput {
+  statusCode: number;
+  // Echo can have any kind of output in addition to standard errors
+  body: string;
+}
+
+export const handler = async (input: string): Promise<ActionOutput> => {
+  try {
+    // Add business logic here
+    const inputAsJson: ActionInput = JSON.parse(input);
+    const { headers, body } = inputAsJson;
+
+    // You can skip headers if you do not need them
+    console.log(headers);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(body),
+    };
+  } catch (e) {
+    console.error(e);
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        errorMessage: e.message,
+        errorType: "",
+      }),
+    };
+  }
+};
---
+++ src/sleep/index.ts
@@ -0,0 +1,47 @@
+/**
+ * Sample action "sleep"
+ * - to demonstrate an async-await delay
+ * - remove if not needed
+ *
+ * Returns the input after a delay as specified by the property "delayTime" in the input
+ */
+import delay from "./delay";
+
+interface ActionInput {
+  body: {
+    delayTime: number;
+  };
+  headers?: {
+    [key: string]: string;
+  };
+}
+
+interface ActionOutput {
+  statusCode: number;
+  body: string;
+}
+
+export const handler = async (input: string): Promise<ActionOutput> => {
+  try {
+    // Add business logic here
+    const inputAsJson: ActionInput = JSON.parse(input);
+    const { body } = inputAsJson;
+    const { delayTime } = body;
+    await delay(delayTime);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ delayTime }),
+    };
+  } catch (e) {
+    console.error(e.stackTrace);
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        errorMessage: e.message,
+        errorType: "",
+      }),
+    };
+  }
+};
