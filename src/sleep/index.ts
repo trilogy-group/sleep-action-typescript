c98d566006ac864cb5d24a7fa2110a92790ad480
amandal-cn <mandal.ashish@codenation.co.in>
2021-08-04 22:03:09 +0530

    whatever

--- src/sleep/index.ts
+++ src/sleep/index.ts
@@ -23,7 +23,6 @@ interface ActionOutput {
 
 export const handler = async (input: string): Promise<ActionOutput> => {
   try {
-
     const inputAsJson: ActionInput = JSON.parse(input);
     const { body } = inputAsJson;
     const { delayTime } = body;
